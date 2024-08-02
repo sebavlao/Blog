93a78a5182e28af0956e8e32e5e485da65eca24b
Videla Sebastián <88522430+sebavlao@users.noreply.github.com>
2024-08-02 14:05:22 -0300

    probando el fetchin de datos con qs library

--- next/src/app/page.js
+++ next/src/app/page.js
@@ -1,13 +1,27 @@
 import { Header } from "./components/header.js";
-import { Form } from "./form.js";
+import qs from "qs";
 
 export default function Home() {
+  const qsObj = {
+    fields: ["title", "body"],
+    populate: {
+      author: {
+        fields: ["name", "lastname"],
+        populate: ["avatar"]
+      },
+      categories: {
+        populate: ["icon"]
+      }
+    }
+  }
+
+  console.log(qs.stringify(qsObj))
+
   return (
     <>
       <Header></Header>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div>
-          <Form></Form>
         </div>
       </main>
     </>
--- next/src/config/config.js
+++
@@ -1,8 +0,0 @@
-import "dotenv";
-
-export const PORT = process.env.PORT;
-
-export const DB_HOST = process.env.DB_HOST;
-export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_NAME = process.env.DB_NAME;
